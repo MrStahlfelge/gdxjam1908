71977357383dd94d97c5efcf83620203e39ebecc
Benjamin Schulte <bs@golfgl.de>
2019-08-25 20:15:09 +0200

    Initial commit, project created freshly

---
+++ desktop/src/de/golfgl/gdxjamgame/oneroom/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.golfgl.gdxjamgame.oneroom.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.golfgl.gdxjamgame.oneroom.GdxJamGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GdxJamGame(), config);
+	}
+}
